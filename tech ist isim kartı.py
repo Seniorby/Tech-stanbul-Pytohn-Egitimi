67ac014aa8cca734d5db0b3f2d14b1e364c311f4
Seniorby <128952658+Seniorby@users.noreply.github.com>
2025-09-17 18:30:06 +0300

    Add files via upload

---
+++ tech ist isim kartı.py
@@ -0,0 +1,10 @@
+#TECH İSTABUL DERSLERİ
+#İSİM KARTI
+ad = input("İsminiz:")
+soyad = input("Soyisminiz:")
+yas = input("Yaşınız:")
+boy = input("Boyunuz:")
+kilo = input("Kilonuz:")
+sehir = input("Yaşadığınız Şehir:")
+hobi = input("Hobi:")
+kursa_devam_durumu = input("Kursa Devam Durumu:")
